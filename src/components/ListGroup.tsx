3df87aa938c7eeb044e02d14b9858cd1e84bb5fc
lauren.crompton <lauren.crompton@coop.co.uk>
2025-12-30 12:17:23 +0000

    added an extra element to the return, added fragment to wrap all the elements

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,15 @@
 function ListGroup() {
-    return (
+    return (<>
+        <h1>List</h1>
         <ul className="list-group">
             <li className="list-group-item">item one</li>
             <li className="list-group-item">item two</li>
             <li className="list-group-item">item three</li>
             <li className="list-group-item">item four</li>
             <li className="list-group-item">item five</li>
-        </ul>);
+        </ul>
+    </>)
+
 }
 
 export default ListGroup;
