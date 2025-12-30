b2fa115dea507325caf8f6922c169a6a4f20f9c1
lauren.crompton <lauren.crompton@coop.co.uk>
2025-12-30 12:13:10 +0000

    added list-group using bootstrap

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,12 @@
+function ListGroup() {
+    return (
+        <ul className="list-group">
+            <li className="list-group-item">item one</li>
+            <li className="list-group-item">item two</li>
+            <li className="list-group-item">item three</li>
+            <li className="list-group-item">item four</li>
+            <li className="list-group-item">item five</li>
+        </ul>);
+}
+
+export default ListGroup;
