0dad803eb6827061f8da4b8456353fc713065a9d
lauren.crompton <lauren.crompton@coop.co.uk>
2025-12-30 15:00:24 +0000

    added function which returns conditional that returns when there's no items in the list

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,17 +1,23 @@
 function ListGroup() {
-    const items = [
+    let items = [
         'New York',
         'San Francisco',
         'Tokyo',
         'London',
         'Paris'
     ];
-
+    items = [];
 //mapping over the items array and converting them into list items
 // to implement the function inside the return, it must be wrapped in curly-braces.
+    function getNoItemsMessage() {
+        return items.length === 0 ? <p>No item found</p> : null;
+    } //it can be beneficial to have a function over a const for a
+    //result as different message may be required depending on circumstance
+
 // Curly-braces make things dynamic
     return (<>
         <h1>List</h1>
+        {getNoItemsMessage()}
         <ul className="list-group">
             {items.map(item =>
                 (<li key={item}>{item}</li>))}
