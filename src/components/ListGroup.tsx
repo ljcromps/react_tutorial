89e500ca51f8e85d0fc0f9ae14659da11adccc9f
lauren.crompton <lauren.crompton@coop.co.uk>
2025-12-30 15:07:53 +0000

    add logical operator to conditional instead (used &&) to avoid returning null anywhere. If the left is true, the right is always true

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -9,15 +9,12 @@ function ListGroup() {
     items = [];
 //mapping over the items array and converting them into list items
 // to implement the function inside the return, it must be wrapped in curly-braces.
-    function getNoItemsMessage() {
-        return items.length === 0 ? <p>No item found</p> : null;
-    } //it can be beneficial to have a function over a const for a
-    //result as different message may be required depending on circumstance
 
 // Curly-braces make things dynamic
+    // logical conditions can help avoid the use of null
     return (<>
         <h1>List</h1>
-        {getNoItemsMessage()}
+        {items.length === 0 && <p>No item found</p>}
         <ul className="list-group">
             {items.map(item =>
                 (<li key={item}>{item}</li>))}
