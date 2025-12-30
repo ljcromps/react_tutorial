6624b68f5af69b63fa5c78ae70a136985459d2d7
lauren.crompton <lauren.crompton@coop.co.uk>
2025-12-30 12:27:11 +0000

    added key to <li> element to allow React to keep track of which elements to update

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -14,11 +14,15 @@ function ListGroup() {
         <h1>List</h1>
         <ul className="list-group">
             {items.map(item =>
-                (<li>{item}</li>))}
+                (<li key={item}>{item}</li>))}
         </ul>
     </>)
 
 }
+
+// <li key={item}> - adding a key to the item allows React to keep
+//track of what should be updated. Usually it will be item.id but
+// just item is okay in this example
 
 export default ListGroup;
 
