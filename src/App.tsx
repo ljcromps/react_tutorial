9490b91caccff2d8eec968dbb8437e835ffd8cbf
lauren.crompton <lauren.crompton@coop.co.uk>
2025-12-31 11:17:23 +0000

    added TS interface object called ListGroupProps to pass data into the ListGroup component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import ListGroup from "./components/ListGroup.tsx";
 
 function App() {
-    return <div><ListGroup/></div>
+    const items = [
+        'New York',
+        'San Francisco',
+        'Tokyo',
+        'London',
+        'Paris'
+    ];
+    return <div><ListGroup items={items} heading={"Cities"}/></div>
 }
 
 export default App;
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,24 +1,17 @@
 import {useState} from "react";
 
-function ListGroup() {
-    const items = [
-        'New York',
-        'San Francisco',
-        'Tokyo',
-        'London',
-        'Paris'
-    ];
+// create an object with two properties: item [] and heading string
+interface ListGroupProps {
+    items: string[];
+    heading: string;
+}
+
+function ListGroup({items, heading}: ListGroupProps) {
+
     const [selectedIndex, setSelectedIndex] = useState(-1);
 
-    // Hook - a function helps us tap into built-in features in React.
-    // We tell React it could have data/state that changes over time
-
-//mapping over the items array and converting them into list items
-// to implement the function inside the return, it must be wrapped in curly-braces.
-// Curly-braces make things dynamic
-    // logical conditions can help avoid the use of null
     return (<>
-        <h1>List</h1>
+        <h1>{heading}</h1>
         {items.length === 0 && <p>No item found</p>}
         <ul className="list-group">
             {items.map((item: string, index: number) =>
